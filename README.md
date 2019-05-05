c6b32ec399d8c4e71b87f809cd7682b9451a41bb
foreverxzhh <foreverxzhh@gmail.com>
2019-05-05 16:38:39 +0800

    Capter 8 finish

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Demo1 for 《Android编程权威指南》
 <h3>Capter7 重要知识点</h3>
 1.使用Fragment<br /> 
+<h3>Capter8 重要知识点</h3>
+1.单例模式<br />
---
+++ app/src/main/java/com/hua/criminalintent/CrimeListFragment.java
@@ -0,0 +1,76 @@
+package com.hua.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+        return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        mAdapter = new CrimeAdapter(crimeLab.getCrimes());
+        mCrimeRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.list_item_crime, viewGroup, false);
+            return new CrimeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder viewHolder, final int i) {
+            viewHolder.mTitleTextView.setText(mCrimes.get(i).getTitle());
+            viewHolder.mDateTextView.setText(mCrimes.get(i).getDate().toString());
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), mCrimes.get(i).getTitle() + "clicked!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = itemView.findViewById(R.id.crime_title);
+            mDateTextView = itemView.findViewById(R.id.crime_date);
+        }
+    }
+}
